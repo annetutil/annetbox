bf89c38c8225473e5321be0ef8f1b577c485c07c
Andrey Tikhonov <tishka17@nebius.com>
2024-05-16 17:50:38 +0200

    Async client for v37

--- example.py
+++ example.py
@@ -1,26 +1,54 @@
+import asyncio
 import os
 
 from annetbox.base.status_client_sync import NetboxStatusClient
+from annetbox.v37.client_async import NetboxV37 as AsyncNetboxV37
 from annetbox.v37.client_sync import NetboxV37
 
 url = os.getenv("NETBOX_URI")
 
-# check the status of netbox installation
-# ClientError with `.status_code == 404` for 2.x versions
-status_client = NetboxStatusClient(url=url)
-status = status_client.status()
-print(status)
 
-# basic netbox methods
-netbox = NetboxV37(url=url)
-res = netbox.devices(limit=1)
-print(res)
-print()
+def main_sync():
+    # check the status of netbox installation
+    # ClientError with `.status_code == 404` for 2.x versions
+    status_client = NetboxStatusClient(url=url)
+    status = status_client.status()
+    print(status)
 
-res = netbox.interfaces(limit=1)
-print(res)
-print()
+    # basic netbox methods
+    netbox = NetboxV37(url=url)
+    res = netbox.devices(limit=1)
+    print(res)
+    print()
 
-res = netbox.ip_addresses(limit=1)
-print(res)
-print()
+    res = netbox.interfaces(limit=1)
+    print(res)
+    print()
+
+    res = netbox.ip_addresses(limit=1)
+    print(res)
+    print()
+
+
+async def main_async():
+    # basic netbox methods
+    netbox = AsyncNetboxV37(url=url)
+    res = await netbox.devices(limit=1)
+    print(res)
+    print()
+
+    res = await netbox.interfaces(limit=1)
+    print(res)
+    print()
+
+    res = await netbox.ip_addresses(limit=1)
+    print(res)
+    print()
+
+    await netbox.close()
+
+
+if __name__ == '__main__':
+    main_sync()
+    print("==== async ===")
+    asyncio.run(main_async())
