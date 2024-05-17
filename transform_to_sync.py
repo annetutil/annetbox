06f5db8dece548810071035123b31e04d3ce395f
Andrey Tikhonov <tishka17@nebius.com>
2024-05-17 17:44:48 +0200

    async to sync transformer

---
+++ transform_to_sync.py
@@ -0,0 +1,23 @@
+"""
+Transformer from async client code to sync one
+
+Usage:
+    python transform_to_sync.py src/annetbox/v37/client_async.py > src/annetbox/v37/client_sync.py
+"""
+import sys
+
+
+def main():
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as f:
+            data = f.read()
+    else:
+        data = sys.stdin.read()
+
+    data = data.replace("annetbox.base.client_async", "annetbox.base.client_sync")
+    data = data.replace("async ", "")
+    print(data, end="")
+
+
+if __name__ == '__main__':
+    main()
