1c81d3f22678c6d3ac5a5ea3f3e5d56e6829823e
Krasotin Pavel <krasotinpa@gmail.com>
2025-06-03 13:47:14 +0300

    Add IP role to NETBOX starting with 3.7

--- src/annetbox/v41/models.py
+++ src/annetbox/v41/models.py
@@ -179,6 +179,7 @@ class IpAddress:
     family: IpFamily
     address: str
     status: Label
+    role: Label | None
     tags: list[EntityWithSlug]
     created: datetime
     last_updated: datetime
